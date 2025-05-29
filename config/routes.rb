00ea029d3cf86bac5ff5bcad3dc3a29cfd661a3d
TheLukeist <lucas@LAPTOP-1U1NNPI8.>
2025-05-28 22:09:38 -0400

    ahora si

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :new, :create, :edit, :update]
-  resources :chats, only: [:index, :show, :new, :create, :edit, :update]
-  resources :messages, only: [:index, :show, :new, :create, :edit, :update]
+  resources :chats, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :messages, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   get "up" => "rails/health#show", as: :rails_health_check
